310d76b408b583b85834e90d284dc04bfe73bf0e
Adam Anderson <adamandersonis@gmail.com>
2009-10-01 14:32:27 -0700

    switching to live() instead of regular click()

--- jquery.omniture.js
+++ jquery.omniture.js
@@ -1,6 +1,6 @@
 jQuery.fn.attachOmnitureClickMap = function(clickmap,options) {
   options = options || {};
-  return this.click(function() {
+  return this.live('click', function() {
     s_objectID = (typeof clickmap === 'function') ? clickmap.call(this) : clickmap
     if(options.remote === true) s.tl(true,'o');
   });
