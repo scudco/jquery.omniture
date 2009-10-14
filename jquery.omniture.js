23443e1463a29f9973141712b78d082501d8ad50
Adam Anderson <adamandersonis@gmail.com>
2009-10-14 10:31:30 -0700

    remote omniture clicks will be logged with Custom Links instead of page-based links. sad day

--- jquery.omniture.js
+++ jquery.omniture.js
@@ -2,7 +2,7 @@ jQuery.fn.attachOmnitureClickMap = function(clickmap,options) {
   options = options || {};
   return this.click(function() {
     s_objectID = (typeof clickmap === 'function') ? clickmap.call(this) : clickmap
-    if(options.remote === true) s.tl(true,'o');
+    if(options.remote === true) s.tl(true,'o',s_objectID);
   });
 };
 
